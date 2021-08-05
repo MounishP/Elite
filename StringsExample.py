c97e15e60e861b9ae016460fb222f4277eabe56d
MounishP <mnsh.pv1@gmail.com>
2021-08-05 13:56:09 +0530

    aug5

---
+++ StringsExample.py
@@ -0,0 +1,46 @@
+# name = 'mounish'
+# for i in range(len(name)):  # range(stop)--> 0 to stop-1
+# print(name[i])
+
+# range(start,stop) --> start to stop-1
+# range(start, stop, step) --> 0,10,1
+# for i in range(0, 20, 3):
+#     print(i)
+# s = 'Python is a fun language to learn'
+# print(s[0:-9])
+# print(s.split("l"))
+
+# s = 'mounish '
+# s = 'ram'
+# print(s)
+
+# s = """Q
+# dslvjndv
+# sdg
+# """
+# print(s)
+
+"""
+input "mounish"
+m
+mo
+mou
+moun
+mouni
+mounis
+mounish
+"""
+
+n = "mounish"
+res = ""
+for i in range(len(n)):
+    res += n[i]
+    print(res)
+
+"""
+    m    
+   mmm   
+  mmmmm  
+ mmmmmmm 
+mmmmmmmmm
+"""
